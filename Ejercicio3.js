8fd232e301fb865ef515fa15f5aa224017daa631
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-16 17:15:37 -0800

    Descripción

--- Ejercicio3.js
+++ Ejercicio3.js
@@ -1,1 +1,4 @@
-Creando archivo
+//Ejercicio 3:
+//Declara dos variables booleanas condicion1 y condicion2.
+//Pide al usuario que ingrese dos valores booleanos (true o false) y 
+//muestra el resultado de diversas combinaciones lógicas.
