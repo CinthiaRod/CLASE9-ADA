2763f56b4d014bde00c00994cc834d50a59a0269
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-16 17:33:02 -0800

    Descripción

--- Ejercicio7.js
+++ Ejercicio7.js
@@ -1,1 +1,4 @@
-Creando archivo
+//Ejercicio 7:
+//Pide al usuario que ingrese su peso en kilogramos y
+// conviértelo a libras. 
+//Muestra el resultado con un mensaje. (Averigua como pasar de kg a libras, Pista: 2.20462)
