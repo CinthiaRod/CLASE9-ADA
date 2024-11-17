d7710383f2c23f3022fd9b9cefb5f3a7b6987c02
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-16 17:37:46 -0800

    Descripción

--- Ejercicio8.js
+++ Ejercicio8.js
@@ -1,1 +1,4 @@
-Creando archivo
+//Ejercicio 8:
+//Pide al usuario que ingrese las longitudes de los tres lados de un triángulo.
+//Determina y muestra si el triángulo es equilátero, isósceles o escaleno.
+// (Investiga sobre los triángulos para determinar la formula)
